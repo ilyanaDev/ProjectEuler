03388f964837ed41e417f49f13adf79a9e12ff0b
Ilyana Smith <ilyana@kentsmiths.com>
2020-06-25 12:19:32 -0400

    solved 4

---
+++ EulerSolver/Solvers/Solver4.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+
+namespace EulerSolver.Solvers
+{
+    public class Solver4 : ISolver
+    {
+        public long FindLargestPalindrome3DigitProduct()
+        {
+
+            int largestSoFar = 0;
+
+            for (int i = 100; i < 1000; i++)
+            {
+                for (int j = 100; j < 1000; j++)
+                {
+                    int temp = i*j;
+
+                    if(this.isPalindrome(temp) && temp > largestSoFar)
+                    {
+                        largestSoFar = temp;
+                    }
+
+                }
+            }
+            return largestSoFar;
+
+        }
+
+        public bool isPalindrome(int num)
+        {
+
+            String numString = num.ToString();
+
+            int length = numString.Length;
+
+            for (int i = 0; i < length/2; i++)
+            {
+                if (numString.Substring(i,1).Equals(numString.Substring(length-(1+i),1)))
+                {
+                    if (i == (length/2)-1)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+           return true;
+
+        }
+
+        string ISolver.Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    }
+
---
+++ EulerSolverTests/Solver4/Solver4Tests.cs
@@ -0,0 +1,41 @@
+﻿using EulerSolver.Solvers;
+using System.Collections;
+using Xunit;
+
+namespace EulerSolverTests
+{
+    public class Solver4Tests
+    {
+
+        [Fact]
+        public void ReturnsTrueGivenPalindrome()
+        {
+            var solver = new Solver4();
+
+            var result = solver.isPalindrome(10001);
+
+            Assert.Equal(true, result);
+        }
+
+        [Fact]
+        public void ReturnsFalseGivenNonPalindrome()
+        {
+            var solver = new Solver4();
+
+            var result = solver.isPalindrome(951598);
+
+            Assert.Equal(false, result);
+        }
+
+        [Fact]
+        public void ReturnsSolution()
+        {
+            var solver = new Solver4();
+
+            var result = solver.FindLargestPalindrome3DigitProduct();
+
+            Assert.Equal(1, result);
+        }
+
+    }
+}
