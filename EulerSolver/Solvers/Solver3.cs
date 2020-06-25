5bb3c3879de50c379f79bf3b8973112228f70c9d
Ilyana Smith <ilyana@kentsmiths.com>
2020-06-25 07:24:12 -0400

    solved #3

---
+++ EulerSolver/Solvers/Solver3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+
+namespace EulerSolver.Solvers
+{
+    public class Solver3 : ISolver
+    {
+        public long FindLargestPrimeFactorOf(long num)
+        {
+            if (this.isPrime(num))
+            {
+                return num;
+            }
+            
+            long largestSoFar = 1;
+
+            for (long i = 2; i < num/2; i++)
+            {
+                if (i != 2 && i % 2 == 0)
+                {
+                    i++;
+                }
+                if (num % i == 0 && this.isPrime(i))
+                {
+                    largestSoFar = i;
+                }
+            }
+
+            return largestSoFar;
+
+        }
+
+        public bool isPrime(long num)
+        {
+            bool result = true;
+
+            for (long i = 2; i < num/2; i++)
+            {
+                if (num % i == 0)
+                {
+                    result = false;
+                    return result;
+                }
+            }
+
+            return result;
+
+        }
+
+        public string Solve()
+        {
+            return "";
+        }
+    }
+}
---
+++ EulerSolverTests/Solver3/Solver3Tests.cs
@@ -0,0 +1,31 @@
+﻿using EulerSolver.Solvers;
+using System.Collections;
+using Xunit;
+
+namespace EulerSolverTests
+{
+    public class Solver3Tests
+    {
+
+        [Fact]
+        public void ReturnsFalseGiven6()
+        {
+            var solver = new Solver3();
+
+            var result = solver.isPrime(6);
+
+            Assert.Equal(false, result);
+        }
+
+        [Fact]
+        public void ReturnsSolution()
+        {
+            var solver = new Solver3();
+
+            var result = solver.FindLargestPrimeFactorOf(600851475143);
+
+            Assert.Equal(1, result);
+        }
+
+    }
+}
