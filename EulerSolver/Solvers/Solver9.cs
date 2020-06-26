903aa04c3e8e8543cdd3e4f2a94c93663b68a1be
Ilyana Smith <ilyana@kentsmiths.com>
2020-06-26 07:36:57 -0400

    solved #9

---
+++ EulerSolver/Solvers/Solver9.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+
+namespace EulerSolver.Solvers
+{
+    public class Solver9 : ISolver
+    {
+        public int FindSolution()
+        {
+            for (int a = 1; a < 1000; a ++)
+            {
+                for (int b = 1; b < 1000; b++)
+                {
+                    for (int c = 1; c < 1000; c++)
+                    {
+                        if (this.isPythag(a,b,c) && this.AddsTo1000(a,b,c))
+                        {
+                            return (a * b * c);
+                        }
+                    }
+                }
+            }
+            
+            int result = 1;
+            
+            return result;
+        }
+
+        public bool isPythag(int a, int b, int c)
+        {
+            if (a == b || a == c || b == c)
+            {
+                return false;
+            }
+            if ((a * a) + (b * b) == (c * c))
+            {
+                return true;
+            }
+            if ((c * c) + (b * b) == (a * a))
+            {
+                return true;
+            }
+            if ((a * a) + (c * c) == (b * b))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool AddsTo1000(int a, int b, int c)
+        {
+            if(a+b+c == 1000)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        string ISolver.Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ EulerSolverTests/Solver9/Solver9Tests.cs
@@ -0,0 +1,41 @@
+﻿using EulerSolver.Solvers;
+using System.Collections;
+using Xunit;
+
+namespace EulerSolverTests
+{
+    public class Solver9Tests
+    {
+
+        [Fact]
+        public void ReturnsSolution()
+        {
+            var solver = new Solver9();
+
+            var result = solver.FindSolution();
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ReturnsTrueGivenPythag()
+        {
+            var solver = new Solver9();
+
+            var result = solver.isPythag(3, 4, 5);
+
+            Assert.Equal(true, result);
+        }
+
+        [Fact]
+        public void ReturnsFalseGivenNonPythag()
+        {
+            var solver = new Solver9();
+
+            var result = solver.isPythag(3, 4, 12);
+
+            Assert.Equal(false, result);
+        }
+
+    }
+}
