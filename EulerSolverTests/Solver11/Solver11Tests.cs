9b53ac2d5e284467e1a22eee9258ca080f785887
Ilyana Smith <ilyana@kentsmiths.com>
2020-06-29 08:09:12 -0400

    solved #11

---
+++ EulerSolverTests/Solver11/Solver11Tests.cs
@@ -0,0 +1,21 @@
+﻿using EulerSolver.Solvers;
+using System.Collections;
+using Xunit;
+
+namespace EulerSolverTests
+{
+    public class Solver11Tests
+    {
+
+        [Fact]
+        public void ReturnsSolution()
+        {
+            var solver = new Solver11();
+
+            var result = solver.FindSolution();
+
+            Assert.Equal(1, result);
+        }
+
+    }
+}
