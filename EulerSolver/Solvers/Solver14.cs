ae86dc95d279ecd854421bdbb40a995c1e9333c9
Ilyana Smith <ilyana@kentsmiths.com>
2020-07-15 13:24:18 -0400

    #14 not yet solved

---
+++ EulerSolver/Solvers/Solver14.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using Tools;
+
+namespace EulerSolver.Solvers
+{
+    public class Solver14 : ISolver
+    {
+        public double FindSolution()
+        {
+            int longestChainLengthSoFar = 351;
+            int varWithLongestChain = 77031;
+
+            //known that 77031, longest chain var under 100,000 is not the longest chain var under 1,000,000
+            for (int incrementVar = 100001; incrementVar < 1000000; incrementVar ++)
+            {
+                //breaks the rule of not messing with for loop incrementation
+                //but I want to test only odd numbers because they should be more likely to be the longest chain
+                incrementVar++;
+
+                int chainLength = this.ChainLength(incrementVar);
+
+                if (chainLength > longestChainLengthSoFar)
+                {
+                    longestChainLengthSoFar = chainLength;
+                    varWithLongestChain = incrementVar;
+                }
+
+            }
+
+            return varWithLongestChain;
+
+        }
+
+        public int ChainLength(int input)
+        {
+            int currentVar = input;
+            int chainLength = 1;
+
+            while (currentVar != 1)
+            {
+
+                if (this.isEven(currentVar))
+                {
+                    currentVar = currentVar / 2;
+
+                }
+
+                else
+                {
+                    currentVar = (currentVar * 3) + 1;
+                    //compressing # of steps; idea found from Wolfram Alpha's reference
+                    //to Terras's modification of the problem
+                    currentVar = currentVar / 2;
+                    chainLength++;
+
+                }
+
+                chainLength++;
+
+            }
+
+            return chainLength;
+
+        }
+
+        public bool isEven(int input)
+        {
+            if (input % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        string ISolver.Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ EulerSolverTests/Solver14/Solver14Tests.cs
@@ -0,0 +1,31 @@
+﻿using EulerSolver.Solvers;
+using System.Collections;
+using Xunit;
+
+namespace EulerSolverTests
+{
+    public class Solver14Tests
+    {
+
+        [Fact]
+        public void ReturnsSolution()
+        {
+            var solver = new Solver14();
+
+            var result = solver.FindSolution();
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ReturnsChainLength()
+        {
+            var solver = new Solver14();
+
+            var result = solver.ChainLength(999993);
+
+            Assert.Equal(1, result);
+        }
+
+    }
+}
