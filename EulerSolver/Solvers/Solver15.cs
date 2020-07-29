4178fef2ee724addf579682e647ea25c707d25e6
Ilyana Smith <ilyana@kentsmiths.com>
2020-07-29 14:54:36 -0400

    solved 15

---
+++ EulerSolver/Solvers/Solver15.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using Tools;
+
+namespace EulerSolver.Solvers
+{
+    public class Solver15 : ISolver
+    {
+        //essential question: how many unique arrangements of 20 rights and 20 downs?
+        public double FindSolution(int sidelengths)
+        {
+
+            double result = this.FindFactorial(sidelengths * 2) / 
+                (this.FindFactorial(sidelengths) * this.FindFactorial(sidelengths));
+
+            return result;
+        }
+
+        public double FindFactorial(int num)
+        {
+            double result = 1;
+            for(int i = 1; i <=  num; i ++)
+            {
+                result = result * i;
+            }
+
+            return result;
+
+        }
+
+        string ISolver.Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ EulerSolverTests/Solver15/Solver15Tests.cs
@@ -0,0 +1,71 @@
+﻿using EulerSolver.Solvers;
+using System.Collections;
+using Xunit;
+
+namespace EulerSolverTests
+{
+    public class Solver15Tests
+    {
+
+        [Fact]
+        public void ReturnsSolution()
+        {
+            var solver = new Solver15();
+
+            var result = solver.FindSolution(20);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Returns6Given2()
+        {
+            var solver = new Solver15();
+
+            var result = solver.FindSolution(2);
+
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void Returns2Given1()
+        {
+            var solver = new Solver15();
+
+            var result = solver.FindSolution(1);
+
+            Assert.Equal(2, result);
+        }
+
+        /**[Fact]
+        public void Returns26Given3()
+        {
+            var solver = new Solver15();
+
+            var result = solver.FindSolution(3);
+
+            Assert.Equal(26, result);
+        }**/
+
+        [Fact]
+        public void FactorialReturns6Given3()
+        {
+            var solver = new Solver15();
+
+            var result = solver.FindFactorial(3);
+
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void FactorialReturns24Given4()
+        {
+            var solver = new Solver15();
+
+            var result = solver.FindFactorial(4);
+
+            Assert.Equal(24, result);
+        }
+
+    }
+}
