e14de71a8e8c81eff728aeb5d22232e322f1a9d0
Ilyana Smith <ilyana@kentsmiths.com>
2020-07-31 14:40:26 -0400

    start of work on 16; not working yet

---
+++ EulerSolver/Solvers/Solver16.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using Tools;
+
+namespace EulerSolver.Solvers
+{
+    public class Solver16 : ISolver
+    {
+        // pattern of last digit of 2 exponents: 2, 4, 8, 6
+        // find sum of digits 2^1000
+
+        public double FindSolution()
+        {
+            double bigNum = Math.Pow(2, 1000);
+
+            double result = 0;
+
+            double remaining = bigNum;
+
+            for(int i = 0; i < 302; i++)
+            {
+                int digValue = (int)(remaining % 10);
+
+                remaining -= digValue;
+                remaining = remaining / 10;
+
+                result += digValue;
+            }
+
+            return result;
+        }
+
+        public double FindNumDigits(double num)
+        {
+            string numString = "" + num;
+
+            double result = numString.Length;
+
+            return result;
+
+        }
+
+        string ISolver.Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ EulerSolverTests/Solver16/Solver16Tests.cs
@@ -0,0 +1,46 @@
+﻿using EulerSolver.Solvers;
+using System;
+using System.Collections;
+using Xunit;
+
+namespace EulerSolverTests
+{
+    public class Solver16Tests
+    {
+
+        [Fact]
+        public void ReturnsSolution()
+        {
+            var solver = new Solver16();
+
+            var result = solver.FindSolution();
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ReturnsNumDigits()
+        {
+            var solver = new Solver16();
+
+            double bigExponent = Math.Pow(2, 1000);
+
+            var result = solver.FindNumDigits(bigExponent);
+
+            Assert.Equal(21, result);
+        }
+
+        [Fact]
+        public void ReturnsNumDigitsProperly()
+        {
+            var solver = new Solver16();
+
+            double bigNum = 1111222333446;
+
+            var result = solver.FindNumDigits(bigNum);
+
+            Assert.Equal(13, result);
+        }
+
+    }
+}
